22f1350adc20d4b9927b37dd7f18bf6bf945baac
Samuel-Fortune <samuelchibu04@gmail.com>
2023-01-02 18:17:57 +0100

    profitfrog

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{css,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+        Barlow: ['Barlow', 'sans-serif'],
+        IBMPlexSans: ['IBM Plex Sans', 'sans-serif'],
+        Inter: ['Inter', 'sans-serif'],
+        Montserrat: ['Montserrat', 'sans-serif'],
+        NotoSans: ['Noto Sans', 'sans-serif'],
+        OpenSans: ['Open Sans', 'sans-serif'],
+        Outfit: ['Outfit', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+        Raleway: ['Raleway', 'sans-serif'],
+        SlaboBig: ['Slabo 27px', 'sans-serif'],
+        Oswald: ['Oswald', 'sans-serif'],
+        Lato: ['Lato', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
