e4e7dc34019a23bdb1283e56a172ad88b5709223
Samuel-Fortune <samuelchibu04@gmail.com>
2023-01-05 12:48:16 +0100

    profitfrog

---
+++ css/script.js
@@ -0,0 +1,22 @@
+//$("#mk").click(function () { 
+//$(".info-menu").toggleClass("active");
+
+
+//});
+
+//$("#mk").mouseover(function () {
+//$(".info-menu").toggleClass("active");
+
+//});
+//$("#mk").mouseleave(function () {
+
+
+
+//});
+
+//$("#mk").mouseover(function () {
+    //$(".info-menu").toggleClass("active");
+    
+
+
+//});
